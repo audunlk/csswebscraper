2fc8bdf9a401769db4a606b94b9cea8797d19604
audunlk <audunlk@gmail.com>
2023-06-13 10:33:33 +0200

    added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,15 +20,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <Script async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2391523060127866"
+          strategy="lazyOnload"
+          crossOrigin="anonymous" />
+          </head>
       <body className={inter.className}>
 
         <div className="fixed top-0 left-0 w-full h-full bg-gradient-to-r from-red-500 via-orange-400 to-yellow-500 -z-10 opacity-80
       "></div>
         {children}
-        <Script async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2391523060127866"
-          strategy="lazyOnload"
-          crossOrigin="anonymous" />
+        
       </body>
     </html>
   )
