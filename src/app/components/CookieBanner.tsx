7b17e396053cff0610e0dcf67186bb1982a5430b
audunlk <audunlk@gmail.com>
2023-06-16 10:24:23 +0200

    added consent banner

--- src/app/components/CookieBanner.tsx
+++ src/app/components/CookieBanner.tsx
@@ -10,7 +10,7 @@ export default function CookieBanner() {
     useEffect(() => {
         const storedCookieConsent = getLocalStorage("cookie_consent", null)
 
-        setCookieConsent(storedCookieConsent)
+        setCookieConsent(false)
     }, [setCookieConsent])
 
 
@@ -31,17 +31,17 @@ export default function CookieBanner() {
             my-10 mx-auto max-w-max md:max-w-screen-sm
                         fixed bottom-0 left-0 right-0 
                         flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-700 rounded-lg shadow`}>
-            <div className='text-center'>
-                <Link href="/info/cookies"><p>We use <span className='font-bold text-sky-400'>cookies</span> on our site.</p></Link>
+                         bg-blue-800 rounded-lg shadow`}>
+            <div className='text-center text-white'>
+                <Link href="/info/cookies"><p>We use <span className='font-bold text-orange-600'>cookies</span> on our site.</p></Link>
             </div>
             <div className='flex gap-2'>
                 <button
                     onClick={() => setCookieConsent(false)}
-                    className='px-5 py-2 text-gray-300 rounded-md border-gray-900'>Decline</button>
+                    className='px-5 py-2 text-white rounded-md border-gray-900'>Decline</button>
                 <button
                     onClick={() => setCookieConsent(true)}
-                    className='bg-gray-900 px-5 py-2 text-white rounded-lg'>Allow Cookies</button>
+                    className='bg-orange-600 px-5 py-2 text-white rounded-lg'>Allow Cookies</button>
             </div>
         </div>
     )
